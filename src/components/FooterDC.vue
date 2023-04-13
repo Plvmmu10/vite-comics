76ff99ac7bfd6c06c1a794c79f57be9de2f67bf2
peppeluc22 <gluca0260@gmail.com>
2023-04-13 18:10:46 +0200

    fixed img alt in bottom footer

--- src/components/FooterDC.vue
+++ src/components/FooterDC.vue
@@ -42,9 +42,9 @@
                 <!-- All social icons -->
                 <img src="/images/footer-facebook.png" alt="facebook">
                 <img src="/images/footer-twitter.png" alt="twitter">
-                <img src="/images/footer-youtube.png" alt="twitter">
-                <img src="/images/footer-pinterest.png" alt="twitter">
-                <img src="/images/footer-periscope.png" alt="twitter">
+                <img src="/images/footer-youtube.png" alt="youtube">
+                <img src="/images/footer-pinterest.png" alt="pinterest">
+                <img src="/images/footer-periscope.png" alt="periscope">
             </div>
         </div>
     </div>
