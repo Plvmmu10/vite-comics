5a04aadcddafe1c321ff65983a40cc0940b2b6fc
peppeluc22 <gluca0260@gmail.com>
2023-04-14 14:41:47 +0200

    added content in main

---
+++ src/components/CardDC.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="col-12 col-md-4 col-lg-2 p-3">
+        <div class="card bg-transparent border-0">
+
+            <div class="card-body">
+                <!-- Card image -->
+                <div class="card-img">
+                    <img :src="thumb" :alt="series" class="img-fluid">
+                </div>
+                <!-- Card title -->
+                <div class="card-title py-2">
+                    <h6 class="text-uppercase">{{ series }}</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['thumb', 'series'],
+    name: 'CardDC'
+}
+</script>
+
+<style lang="scss" scoped>
+.card-img {
+    width: 100%;
+    height: 200px;
+
+    img {
+        width: 100%;
+        height: 100%;
+    }
+}
+</style>
--- src/components/MainDC.vue
+++ src/components/MainDC.vue
@@ -1,12 +1,22 @@
 <template>
-    <div class="myContainer text-white">
-        <h3> Content goes here</h3>
+    <div class="myContainer text-white flex-wrap">
+        <CardDC :thumb="item.thumb" :series="item.series" v-for="item in comicsList" />
     </div>
 </template>
 
 <script>
+import CardDC from './CardDC.vue';
+import { comics } from '../assets/data/data.js';
 export default {
-
+    name: "MainDC",
+    components: {
+        CardDC
+    },
+    data() {
+        return {
+            comicsList: [...comics]
+        }
+    }
 }
 </script>
 
