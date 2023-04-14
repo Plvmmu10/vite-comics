b39912f03c900c4e7b09ac18175b660aa584c0f9
peppeluc22 <gluca0260@gmail.com>
2023-04-14 14:47:45 +0200

    added button in main

--- src/components/MainDC.vue
+++ src/components/MainDC.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="myContainer text-white flex-wrap">
+        <span class="text-uppercase fw-bold">current series</span>
         <CardDC :thumb="item.thumb" :series="item.series" v-for="item in comicsList" />
+        <div class="btn-container d-flex justify-content-center align-items-center w-100">
+            <button class="btn btn-primary text-uppercase rounded-0 fw-bold">load more</button>
+        </div>
+
     </div>
 </template>
 
@@ -23,5 +28,13 @@ export default {
 <style lang="scss" scoped>
 .myContainer {
     padding: 2rem 0;
+    position: relative;
+
+    span {
+        position: absolute;
+        top: -20px;
+        padding: 1rem;
+        background-color: #0282f9;
+    }
 }
 </style>
